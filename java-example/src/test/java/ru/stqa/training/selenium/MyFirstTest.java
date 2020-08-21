de83a6d6b4f8e7834909396e45cb5fcef3bda955
alexeevaelena06 <alexeevaelena06@gmail.com>
2020-08-21 12:35:57 +0300

    change MyFirstTest

--- java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -4,31 +4,33 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
-
 
 public class MyFirstTest {
     private WebDriver driver;
-    private Wait<WebDriver> wait;
+//    private Wait<WebDriver> wait;
 
     @Before
     public  void start() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10)).withMessage("Element was not found");
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(10)).withMessage("Element was not found");
     }
 
     @Test
     public void myFirstTest() {
         driver.get("http://www.google.com/");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnK")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));
+        driver.findElement(By.name("q")).sendKeys("webdriver" + Keys.ENTER);
+        Actions actionProvider = new Actions(driver);
+        Action keydown = actionProvider.keyDown(Keys.CONTROL).sendKeys("a").build();
+        keydown.perform();
 
     }
 
